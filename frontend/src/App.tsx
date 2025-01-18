ba0bf91eb7cdade6756754a54f3d89e825b367c2
Navya <nj255@snu.edu.in>
2025-01-18 13:24:42 +0530

    start

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Pages from "./pages";
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to My App</h1>
+            <Pages />
+        </div>
+    );
+}
+
+export default App;
